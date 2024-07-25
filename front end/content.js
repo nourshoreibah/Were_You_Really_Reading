326b3b2ecdabddcb473d192fdea1d7f6a11e3ee5
Nour Shoreibah <168875317+nourshoreibah@users.noreply.github.com>
2024-07-25 20:16:50 +0300

    Update content.js

--- front end/content.js
+++ front end/content.js
@@ -1,3 +1,4 @@
+// The content script portion of the Chrome extension is able to interact with the webpages the user has loaded
 console.log("Content script loaded");
 function extractText() {
     return document.body.innerText;
